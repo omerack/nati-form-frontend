2c2b40b9fb90ee6c83e2e711bf94ff9172528907
omerack <omeracker1@gmail.com>
2023-12-17 19:00:30 +0200

    new pdf and changes

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -42,82 +42,8 @@ function PersonalInfo() {
             },
           })}
         />
-        <TextField
-          InputProps={{}}
-          sx={{ mt: 4, width: "350px" }}
-          id="outlined-basic"
-          label="מספר טלפון"
-          variant="outlined"
-          {...register("phone", {
-            required: "נא למלא את מספר הטלפון",
-            validate: {
-              length: (fieldValue) => {
-                return (
-                  fieldValue.length === 10 || "מספר הנייד חייב להיות 10 ספרות"
-                );
-              },
-            },
-          })}
-        />
         {errors.id && <Alert severity="error">{errors.id.message}</Alert>}
-        {errors.phone && <Alert severity="error">{errors.phone.message}</Alert>}
         <Genus />
-        <label>דוא"ל</label>
-        <TextField
-          id="outlined-basic"
-          label="someone@example.com"
-          variant="outlined"
-          sx={{ width: "350px" }}
-          {...register("email", {
-            required: "נא למלא את הדואל",
-            pattern: {
-              value:
-                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: "אימייל לא תקני",
-            },
-          })}
-        />
-        {errors.email && <Alert severity="error">{errors.email.message}</Alert>}
-        <label style={{ margin: "25px 0" }}>כתובת מגורים</label>
-        <TextField
-          style={{ width: "300px", marginLeft: "10px", marginBottom: "20px" }}
-          id="outlined-basic"
-          label="רחוב"
-          variant="outlined"
-          {...register("street", {
-            required: "נא למלא את הרחוב",
-          })}
-        />
-        <TextField
-          style={{ width: "100px", marginLeft: "20px" }}
-          id="outlined-basic"
-          label="מספר"
-          variant="outlined"
-          {...register("streetNumber", {
-            required: "נא למלא מספר בית",
-            pattern: {
-              value: /^[0-9]+$/,
-              message: "לא מספר",
-            },
-          })}
-        />
-        <TextField
-          style={{ width: "200px" }}
-          id="outlined-basic"
-          label="עיר"
-          variant="outlined"
-          fullWidth
-          {...register("city", {
-            required: "נא למלא את שם העיר",
-          })}
-        />
-        {errors.street && (
-          <Alert severity="error">{errors.street.message}</Alert>
-        )}
-        {errors.city && <Alert severity="error">{errors.city.message}</Alert>}
-        {errors.streetNumber && (
-          <Alert severity="error">{errors.streetNumber.message}</Alert>
-        )}
       </div>
     </div>
   );
