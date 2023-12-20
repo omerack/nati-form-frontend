6a0243bb87178d44c09921dba9b443c64c73611e
omerack <omeracker1@gmail.com>
2023-12-20 20:53:55 +0200

    test

--- src/utils/AuthContext.js
+++ src/utils/AuthContext.js
@@ -19,6 +19,7 @@ export const AuthProvider = ({ children }) => {
 
     try {
       let response = await account.createEmailSession(
+        userInfo.userId,
         userInfo.email,
         userInfo.password
       );
