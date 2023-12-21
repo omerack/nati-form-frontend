df0d6f6f743d633b0e269149f75a1ca073702db2
omerack <omeracker1@gmail.com>
2023-12-21 10:53:58 +0200

    association added

--- src/components/Private.js
+++ src/components/Private.js
@@ -1,11 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Alert from "@mui/material/Alert";
 import { useFormContext } from "react-hook-form";
+import Married from "./Married";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
 function Private() {
   const { register, formState } = useFormContext();
   const { errors } = formState;
+
+  const [maritalStatus, setMaritalStatus] = useState("singal");
+
+  const handleMaritalStatusChange = (event) => {
+    setMaritalStatus(event.target.value);
+  };
 
   return (
     <div>
@@ -56,6 +68,33 @@ function Private() {
         />
         {errors.id && <Alert severity="error">{errors.id.message}</Alert>}
       </div>
+      <FormControl>
+        <FormLabel>מצב משפחתי</FormLabel>
+        <RadioGroup
+          row
+          value={maritalStatus}
+          onChange={handleMaritalStatusChange}
+        >
+          <FormControlLabel value="singal" control={<Radio />} label="רווק" />
+          <FormControlLabel value="married" control={<Radio />} label="נשוי" />
+          <FormControlLabel value="divorced" control={<Radio />} label="גרוש" />
+          <FormControlLabel value="widower" control={<Radio />} label="אלמן" />
+          <FormControlLabel
+            value="public"
+            control={<Radio />}
+            label="ידוע בציבור"
+          />
+          <FormControlLabel
+            value="separated"
+            control={<Radio />}
+            label="פרוד"
+          />
+        </RadioGroup>
+      </FormControl>
+
+      {maritalStatus === "married" && (
+        <Married register={register} errors={errors} />
+      )}
     </div>
   );
 }
--- src/components/WhichClient.js
+++ src/components/WhichClient.js
@@ -4,17 +4,13 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
-import Married from "./Married";
 import Private from "./Private";
 import Association from "./Association";
 
-function PersonalInfo({ register, errors }) {
-  const [maritalStatus, setMaritalStatus] = useState("singal");
+function PersonalInfo() {
   const [client, setClient] = useState("private");
 
-  const handleMaritalStatusChange = (event) => {
-    setMaritalStatus(event.target.value);
-  };
+
 
   const handleClientChange = (event) => {
     setClient(event.target.value);
@@ -39,34 +35,6 @@ function PersonalInfo({ register, errors }) {
         </RadioGroup>
       </FormControl>
       {client === "private" ? <Private /> : <Association />}
-
-      <FormControl>
-        <FormLabel>מצב משפחתי</FormLabel>
-        <RadioGroup
-          row
-          value={maritalStatus}
-          onChange={handleMaritalStatusChange}
-        >
-          <FormControlLabel value="singal" control={<Radio />} label="רווק" />
-          <FormControlLabel value="married" control={<Radio />} label="נשוי" />
-          <FormControlLabel value="divorced" control={<Radio />} label="גרוש" />
-          <FormControlLabel value="widower" control={<Radio />} label="אלמן" />
-          <FormControlLabel
-            value="public"
-            control={<Radio />}
-            label="ידוע בציבור"
-          />
-          <FormControlLabel
-            value="separated"
-            control={<Radio />}
-            label="פרוד"
-          />
-        </RadioGroup>
-      </FormControl>
-
-      {maritalStatus === "married" && (
-        <Married register={register} errors={errors} />
-      )}
     </div>
   );
 }
