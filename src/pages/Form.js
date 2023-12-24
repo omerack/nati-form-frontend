6edf02007c106ab8b56f9a7fade5f4a67f15991c
omerack <omeracker1@gmail.com>
2023-12-24 21:28:28 +0200

    gilad stamp is added after sending the email

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -15,21 +15,19 @@ import { useAuth } from "../utils/AuthContext";
 import { Alert } from "@mui/material";
 
 function Form() {
-  const methods = useForm();
-
-  // {
-  //   defaultValues: {
-  //     associationName: "איציק בעמ",
-  //     name: "עומר",
-  //     lastName: "אקרמן",
-  //     id: "204942049",
-  //     phone: "0546229546",
-  //     email: "omeracker1@gmail.com",
-  //     street: "יהודה הלוי",
-  //     streetNumber: "12",
-  //     city: "נתניה",
-  //   },
-  // }
+  const methods = useForm({
+    defaultValues: {
+      // associationName: "איציק בעמ",
+      name: "עומר",
+      lastName: "אקרמן",
+      id: "204942049",
+      phone: "0546229546",
+      email: "omeracker1@gmail.com",
+      street: "יהודה הלוי",
+      streetNumber: "12",
+      city: "נתניה",
+    },
+  });
 
   const { register, handleSubmit, formState } = methods;
   const { errors } = formState;
@@ -65,7 +63,7 @@ function Form() {
       }
       console.log(data);
 
-      await axios.post(`http://localhost:3001/view`, data, {
+      await axios.post(`https://gilad-form-backend.onrender.com/view`, data, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
